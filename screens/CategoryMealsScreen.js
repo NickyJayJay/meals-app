9edee093a8d1c4ad0d5af4acba93752aae75e86d
Nick Jacob <njacob341@gmail.com>
2021-09-01 11:55:56 -0700

    Passing and reading params upon navigation

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
 
+import { CATEGORIES } from '../data/dummy-data';
+
 const CategoryMealScreen = (props) => {
+  const catId = props.navigation.getParam('categoryId');
+
+  const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
-      <Button title='Go to Details!' onPress={() => {
-          props.navigation.navigate({routeName: 'MealDetail'});
-      }} />
-      <Button title='Go Back' onPress={() => {
+      <Text>{selectedCategory.title}</Text>
+      <Button
+        title='Go to Details!'
+        onPress={() => {
+          props.navigation.navigate({ routeName: 'MealDetail' });
+        }}
+      />
+      <Button
+        title='Go Back'
+        onPress={() => {
           props.navigation.pop();
-      }} />
+        }}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default CategoryMealScreen;
