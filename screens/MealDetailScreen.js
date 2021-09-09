c16c5f37ad23dfe7c48c811325e43c9d8e46bd95
Nick Jacob <njacob341@gmail.com>
2021-09-09 10:43:24 -0700

    Selecting state slices

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,28 +1,24 @@
 import React from 'react';
-import {
-  View,
-  Text,
-  Button,
-  StyleSheet,
-  ScrollView,
-  Image,
-} from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
-import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
 
-const ListItem = props => {
-  return <View style={styles.listItem}>
-    <DefaultText>{props.children}</DefaultText>
-  </View>
-}
+const ListItem = (props) => {
+  return (
+    <View style={styles.listItem}>
+      <DefaultText>{props.children}</DefaultText>
+    </View>
+  );
+};
 
 const MealDetailScreen = (props) => {
+  const availableMeals = useSelector((state) => state.meals.meals);
   const mealId = props.navigation.getParam('mealId');
 
-  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  const selectedMeal = availableMeals.find((meal) => meal.id === mealId);
   return (
     <ScrollView>
       <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
@@ -82,8 +78,8 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     borderColor: '#ccc',
     borderWidth: 1,
-    padding: 10
-  }
+    padding: 10,
+  },
 });
 
 export default MealDetailScreen;
